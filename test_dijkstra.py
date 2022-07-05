f78fdbc1d7584dfabc25980b9266fa68d42ace86
milevskyi <dmytro.milevskyi@geocomply.com>
2022-07-05 21:20:35 +0300

    Run some test cases

--- test_dijkstra.py
+++ test_dijkstra.py
@@ -4,7 +4,7 @@ from dijkstra import Graph
 
 
 class DijkstraTest(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
 
         self.graph = Graph(
             [
@@ -17,8 +17,24 @@ class DijkstraTest(unittest.TestCase):
                 ("c", "f", 2),
                 ("d", "e", 6),
                 ("e", "f", 9),
+                ("f", "g", float("inf"))
             ]
         )
 
     def test(self):
         self.assertEqual(['a', 'b'], list(self.graph.dijkstra("a", "b")))
+
+    def test_shortest_route(self):
+        self.assertEqual(['a', 'c', 'f', 'e'], list(self.graph.dijkstra("a", "e")))
+
+    def test_no_path(self):
+        self.assertEqual(['g'], list(self.graph.dijkstra("a", "g")))
+
+    def test_same_cost(self):
+        self.assertEqual(['e', 'f'], list(self.graph.dijkstra("e", "f")))
+
+    def test_non_existent_vertices(self):
+        with self.assertRaises(KeyError) as context:
+            list(self.graph.dijkstra("a", "z"))
+
+        self.assertFalse('There is undefined vertices' in context.exception)
